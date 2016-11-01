7eb799ba315d8788ecac7006466eb08d6efae6d8
colinomay <colin.may1@marist.edu>
2016-11-01 10:38:37 -0400

    Add files via upload

--- helpers.php
+++ helpers.php
@@ -39,6 +39,75 @@ function show_records($dbc) {
    mysqli_free_result( $results ) ;
  }
 }
+function show_link_records($dbc) {
+	# Create a query to get the number, fname, lname sorted by number
+	$query = 'SELECT number, lname FROM presidents ORDER BY number ASC' ;
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+	# Show results
+	if( $results )
+ 	{
+	   # But...wait until we know the query succeeded before
+   # starting the table.
+   echo '<H1>Dead Presidents</H1>' ;
+   echo '<TABLE>';
+   echo '<TR>';
+   echo '<TH>Number</TH>';
+   echo '<TH>Last Name</TH>';
+   echo '</TR>';
+  # For each row result, generate a table row
+   while ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+   {
+     echo '<TR>' ;
+     $alink = '<A HREF=linkypresidents.php?number=' . $row['number']
+        . '>' . $row['number'] . '</A>' ;
+     echo '<TD ALIGN=center>' . $alink . '</TD>' ;
+     echo '<TD>' . $row['lname'] . '</TD>' ;
+    
+     echo '</TR>' ;
+   }
+    
+  # End the table
+    echo '</TABLE>';
+  # Free up the results in memory
+   mysqli_free_result( $results ) ;
+ }
+}
+function show_record($dbc, $number) {
+	# Create a query to get the number, fname, lname sorted by number
+	$query = 'SELECT number, lname, fname FROM presidents WHERE number = ' . $number ;
+
+	# Execute the query
+	$results = mysqli_query( $dbc , $query ) ;
+	check_results($results) ;
+	# Show results
+	if( $results )
+ 	{
+	   # But...wait until we know the query succeeded before
+   # starting the table.
+   echo '<H1>Dead Presidents</H1>' ;
+   echo '<TABLE>';
+   echo '<TR>';
+   echo '<TH>Number</TH>';
+   echo '<TH>First Name</TH>'; 
+   echo '<TH>Last Name</TH>';
+   echo '</TR>';
+  # For each row result, generate a table row
+   if ( $row = mysqli_fetch_array( $results , MYSQLI_ASSOC ) )
+   {
+     echo '<TR>' ;
+     echo '<TD>' . $row['number'] . '</TD>' ;
+     echo '<TD>' . $row['fname'] . '</TD>' ;
+     echo '<TD>' . $row['lname'] . '</TD>' ;
+     echo '</TR>' ;
+   }
+  # End the table
+   echo '</TABLE>';
+  # Free up the results in memory
+   mysqli_free_result( $results ) ;
+ }
+}
 # Inserts a record into the prints table
 function insert_record($dbc, $number, $fname, $lname) {
   $query = 'INSERT INTO presidents(number, fname, lname) VALUES (' . $number . ' , "' . $fname . '" , "' . $lname . '")' ;
